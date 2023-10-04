59b2690b4aa802572767673f5cb020d2965b04be
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-10-04 19:09:55 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+/*function nextTrack() {
+    var currentIndexInput = document.querySelector('input[name="indexValue"]');
+    var currentIndex = parseInt(currentIndexInput.value);
+    currentIndex += 1; // Increment the current index
+    currentIndexInput.value = currentIndex; // Update the input value
+    document.querySelector('form').submit(); // Submit the form
+}*/
+ 
