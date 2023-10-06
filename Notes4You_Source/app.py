c48003f6524a08b6e44ab43a94246e389dfb63cc
Deep Patel <103757105+deep-patel21@users.noreply.github.com>
2023-10-05 23:07:34 -0400

    Add files via upload

---
+++ Notes4You_Source/app.py
@@ -0,0 +1,135 @@
+from flask import Flask, jsonify, request, url_for, redirect, session, render_template
+#from flask_cors import CORS
+from  spotipy.oauth2 import SpotifyOAuth
+import spotipy
+
+import time
+
+#Initate Flask application
+#Implemented Cross-Origin Resource Sharing 
+app  = Flask(__name__)
+#CORS(app)
+
+app.secret_key = "placeholder"
+app.config["SESSION_COOKIE_NAME"] = "notes4you-login-session"
+
+TOKEN_INFO = "token_info"
+#Demonstrates a collection of songs for which piano tutorials are easily available
+PLAYLIST_ID = "https://open.spotify.com/playlist/2WuGvRE6iBfVNhMViK20Ne?si=c97ea55959f84879"
+#Demonstrates alternate user flow, for playlists in which piano tutorials are rare
+PLAYLIST_ID2 = "https://open.spotify.com/playlist/6vXUl7oiMMwIcBQzFfpbAp?si=b19e8bfacbd84de9"
+track_state_position = None
+
+#Endpoint Usage: fetches authorization_url from Spotify
+#Parameters Returned: Throws redirect for user to authorize on Spotify servers
+@app.route("/")
+def login():
+    sp_oauth = create_spotify_oauth()
+    authorization_url = sp_oauth.get_authorize_url()
+    print(authorization_url)
+    redirect(authorization_url)
+    return render_template('index.html')
+
+#Endpoint Usage: User to Spotify server
+#                Spotify server to redirect_uri at http://localhost:5000/redirect/
+#                                                  http://localhost:5000/redirect
+#                                                  http://127.0.0.1:5000/redirect/
+#                                                  http://127.0.0.1:5000/redirect
+#Parameters Returned: Throws redirect to /getTrack endpoint
+@app.route('/redirect')
+def redirectPage():
+    sp_oauth = create_spotify_oauth()
+    session.clear()
+    code = request.args.get("code")
+    token_info = sp_oauth.get_access_token(code)
+    session[TOKEN_INFO] = token_info
+    return redirect("/getTrack")
+
+def get_token():
+    token_info = session.get(TOKEN_INFO, None)
+    if not token_info:
+        raise "exception"
+    current_time = int(time.time())
+    is_expired = (token_info["expires_at"] - current_time) < 60
+    if is_expired:
+        sp_oauth = create_spotify_oauth()
+        token_info = sp_oauth.refresh_access_token(token_info["refresh_token"])
+    return token_info
+
+#Endpoint Usage: Internal
+#Parameters Returned: In-built CSS formatted display string containing 
+#                      track_name, artist(s), and cover image of track
+@app.route('/getTrack', methods=['POST', 'GET'])
+def getTrack():
+    try:
+        token_info = get_token()
+    except:
+        print("user not logged in yet")
+        return redirect("/")
+    sp = spotipy.Spotify(auth = token_info["access_token"])
+    
+    devices = sp.devices()
+    if devices['devices']:
+        device_id = devices['devices'][0]['id']
+
+    results = sp.playlist_tracks(PLAYLIST_ID, limit = 15)
+    tracks_info = []
+    for track in results["items"]:
+        track_name = track["track"]["name"]
+        artists = [artist["name"] for artist in track["track"]["artists"]]
+        cover_art_url = track["track"]["album"]["images"][0]["url"]
+        details_dict = {
+            "track_name": track_name, 
+            "artists": ", ".join(artists),
+            "cover_art_url": cover_art_url
+        }
+        tracks_info.append(details_dict) 
+
+    current_track_index = session.get('current_track_index_html', 0)
+        
+    if request.method == 'POST':
+        if "next-button" in request.form:
+            current_track_index += 1
+            session['current_track_index_html'] = current_track_index
+            print("Updated Track Index: " + str(current_track_index))
+            track_uri = results['items'][current_track_index]["track"]["uri"]
+            sp.start_playback(uris=[track_uri], device_id=device_id)
+        elif 'previous-button' in request.form:
+            current_track_index -= 1
+            session['current_track_index_html'] = current_track_index
+            print("Updated Track Index: " + str(current_track_index))
+            track_uri = results['items'][current_track_index]["track"]["uri"]
+            sp.start_playback(uris=[track_uri], device_id=device_id)
+        elif 'play-button' in request.form:
+            track_state = sp.current_playback()
+            if track_state and track_state['is_playing']:
+                sp.pause_playback(device_id = device_id)
+            else:
+                track_uri = results['items'][current_track_index]["track"]["uri"]
+                sp.start_playback(uris = [track_uri], device_id = device_id)
+
+    current_track_index = min(current_track_index, len(tracks_info) - 1)
+
+    return render_template('index.html', access_token=token_info["access_token"],
+            cover_art_url_html = tracks_info[current_track_index]["cover_art_url"], 
+                track_name_html = tracks_info[current_track_index]["track_name"], 
+                    artist_name_html = tracks_info[current_track_index]["artists"], 
+                        current_track_index_html = current_track_index)
+    
+
+@app.route('/logout')
+def logout():
+    for key in list(session.keys()):
+        session.pop(key)
+    return redirect("/")
+
+#Method Usage: Implement Spotify OAuthentication procedure
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id = "placeholder",
+        client_secret = "placeholder",
+        redirect_uri = url_for("redirectPage", _external = True),
+        scope = "user-library-read, user-modify-playback-state, user-read-playback-state")
+    
+if __name__  == "__main__":
+    app.run(host = "localhost")
---
+++ Notes4You_Source/static/script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var accessToken = "{{ access_token }}";
+    let query = document.querySelector('.query');
+    let search = document.querySelector('.search');
+
+    function loadYouTubeVideo() {
+        let trackName = document.getElementById('track-name').textContent;
+        let artistName = document.getElementById('artist-name').textContent;
+        let searchQuery = `${trackName} ${artistName} piano tutorial youtube`;
+
+        new YT.Player(videoFrame, {
+            height: '497.81',
+            width: '885',
+            videoId: '',
+            playerVars: {
+                'origin': window.location.origin,
+                'enablejsapi': 1
+            },
+            events: {
+                'onReady': onPlayerReady
+            }
+        });
+
+        function onPlayerReady(event) {
+        }
+
+        let url = "https://www.youtube.com/search?q=" + trackName + " " + artistName + " midipianotutorial";
+        window.open(url, '_blank');
+    }
+
+    search.onclick = loadYouTubeVideo;
+
+    function searchYouTubeVideo(trackName, artistName) {
+        const apiKey = "placeholder";
+        const query = `${trackName} ${artistName} piano tutorial`;
+        const maxResults = 1;
+
+        const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${apiKey}&part=snippet&type=video&maxResults=${maxResults}&q=${query}`;
+
+        fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            const videoId = data.items[0].id.videoId;
+            const iframe = document.getElementById('video-frame');
+            iframe.src = `https://www.youtube.com/embed/${videoId}`;
+        })
+        .catch((error) => {
+            console.error('Error fetching YouTube data:', error);
+        });
+    }
+
+    let trackName = document.getElementById('track-name').textContent;
+    let artistName = document.getElementById('artist-name').textContent;
+    let videoFrame = document.getElementById('video-frame');
+    searchYouTubeVideo(trackName, artistName);
+});
+
